093d2f87357d8e4fee6907c1e5098adb7370b7a5
nikhil <nikhildhumal2001@gmaail.com>
2024-08-04 23:32:36 +0530

    homepage and postgre configurations also added .env

--- foodonline_main/views.py
+++ foodonline_main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(response):
-    return HttpResponse('NIkhil')
+def home(request):
+    return render( request, 'home.html')
