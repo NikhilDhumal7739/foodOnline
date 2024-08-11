cec87a71a51d9e77537d4d524ba3101b46333986
nikhil <nikhildhumal2001@gmaail.com>
2024-08-12 00:51:18 +0530

    register user functionalities and homepaage frontend modification

--- foodonline_main/urls.py
+++ foodonline_main/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for foodonline_main project.
+URL configuration for foodOnline_main project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -15,10 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name = 'home')
-]
+    path('', views.home, name = 'home'),
+    path('accounts/', include('accounts.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
