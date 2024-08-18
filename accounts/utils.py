1367a0aef2c339279cbc8bc64e9b45f339669dab
nikhil <nikhildhumal2001@gmaail.com>
2024-08-18 18:44:49 +0530

    customer and vendor registration

---
+++ accounts/utils.py
@@ -0,0 +1,12 @@
+#only for helper functions
+
+def detectUser(user):
+    if user.role == 1:
+        redirectUrl = 'vendordashboard'
+        return redirectUrl
+    elif user.role == 2:
+        redirectUrl = 'custdashboard'
+        return redirectUrl
+    elif user.role == None and user.is_superadmin:
+        redirectUrl = '/admin'
+        return redirectUrl
--- accounts/views.py
+++ accounts/views.py
@@ -4,12 +4,33 @@ from django.shortcuts import redirect, render
 from vendor.forms import VendorForm
 from .forms import UserForm
 from .models import User, UserProfile
-from django.contrib import messages
+from django.contrib import messages, auth
+from.utils import detectUser
+from django.contrib.auth.decorators import login_required, user_passes_test
 
-# Create your views here.
+from django.core.exceptions import PermissionDenied
+
+
+#Restrict the vendor from accessing customer page
+def check_role_vendor(user):
+    if user.role==1:
+        return True
+    else:
+        raise PermissionDenied
+
+
+#Restrict the customer from accessing vendor page
+def check_role_customer(user):
+    if user.role==2:
+        return True
+    else:
+        raise PermissionDenied
 
 def registerUser(request):
-    if request.method == 'POST':
+    if request.user.is_authenticated:
+        messages.warning(request, 'You are already logged in')
+        return redirect('dashboard')#CHECk THIS  LINE
+    elif request.method == 'POST':
         print(request.POST)
         form = UserForm(request.POST)
         if form.is_valid():
@@ -43,7 +64,10 @@ def registerUser(request):
     return render(request, 'accounts/registerUser.html', context)
 
 def registerVendor(request):
-    if request.method == 'POST':
+    if request.user.is_authenticated:
+        messages.warning(request, 'You are already logged in')
+        return redirect('dashboard')
+    elif request.method == 'POST':
         #store data and create user
         form = UserForm(request.POST)
         v_form = VendorForm(request.POST, request.FILES)
@@ -61,7 +85,7 @@ def registerVendor(request):
             user_profile = UserProfile.objects.get(user=user)
             vendor.user_profile = user_profile
             vendor.save()
-            messages.success(request, 'account created. Please wait for approvel')
+            messages.success(request, 'account created. Please wait for approval')
             return redirect('registerVendor')
         else:
             print('Invalid form')
@@ -76,3 +100,45 @@ def registerVendor(request):
     }
 
     return render(request, 'accounts/registerVendor.html', context)
+
+def login(request):
+    if request.user.is_authenticated:
+        messages.warning(request, 'You are already logged in')
+        return redirect('myAccount')
+    elif request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = auth.authenticate(email=email, password=password)
+
+        if user is not None:
+            auth.login(request,user)
+            messages.success(request, 'You are now logged in.')
+            return redirect('myAccount')
+        else:
+            messages.error(request, 'Invalid login credentials')
+            return redirect('login')
+    return render(request, 'accounts/login.html')
+
+def logout(request):
+    auth.logout(request)
+    messages.info(request, 'You are logged out')
+    return redirect('login')
+
+@login_required(login_url='login')
+
+def myAccount(request):
+    user = request.user
+    redirectUrl = detectUser(user)
+    return redirect(redirectUrl)
+
+@login_required(login_url='login')
+@user_passes_test(check_role_customer)
+def custdashboard(request):
+    return render(request, 'accounts/custdashboard.html')
+
+@login_required(login_url='login')
+@user_passes_test(check_role_vendor)
+
+def vendordashboard(request):
+    return render(request, 'accounts/vendordashboard.html')
